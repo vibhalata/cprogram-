0075af983d6809d6769191dbcee473e1c0ae60b3
vibhalata <vibhalatabanarsi219@gmail.com>
2025-08-19 15:13:56 +0530

    comment uploaded

---
+++ conditional.c
@@ -0,0 +1,11 @@
+#include<studio.h;>
+int main()
+{
+    int n,r;
+    printf("enter a number");
+    scanf("enter %d a number & n");
+    r=n%10;
+    if(r==5){printf("yes")}else
+    printf("no");
+
+}
